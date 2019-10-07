f823e40168753eb1ae02b27d442ede07f2ec7b67
clancy.meade <clancy.meade@mu-c1-16.mullen.pvt.k12.co.us>
2019-10-07 12:21:58 -0600

    Commit from class 10/7/19

--- Chord Generator/src/ChordGeneratorDriver.java
+++ Chord Generator/src/ChordGeneratorDriver.java
@@ -1,11 +1,17 @@
+import java.util.ArrayList;
 
 public class ChordGeneratorDriver
 	{
-	
+		static ArrayList <keySignature> keySignatures = new ArrayList <keySignature>();
 
 		public static void main(String[] args)
 			{
+				
+				keySignatures.add(new keySignature ("a", "b", "c", "d","a", "b", "c", "d","a", "b", "c", "d","a", "a")); 
+				
 				ChordGeneratorGreetUser.introduceGenerator(); 
+				
+				
 			}
 
 	}
