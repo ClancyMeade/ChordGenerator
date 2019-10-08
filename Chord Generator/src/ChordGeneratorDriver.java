fec889dbc41a6ca63b1f2f0fd45ca20f4361c32a
clancy.meade <clancy.meade@mu-c1-16.mullen.pvt.k12.co.us>
2019-10-08 11:56:26 -0600

    Commit from class 10/8/19

--- Chord Generator/src/ChordGeneratorDriver.java
+++ Chord Generator/src/ChordGeneratorDriver.java
@@ -6,10 +6,18 @@ public class ChordGeneratorDriver
 
 		public static void main(String[] args)
 			{
-				
-				keySignatures.add(new keySignature ("a", "b", "c", "d","a", "b", "c", "d","a", "b", "c", "d","a", "a")); 
+			
+				//C Major = no sharps or flats
+				keySignatures.add(new keySignature ("CM", "Cm", "DM", "Dm","EM", "Em", "FM", "Fm","GM", "Gm", "AM", "Am","BM", "Bm")); 
+				//G Major = one sharp
+				keySignatures.add(new keySignature ("GM", "Gm", "AM", "Am", "BM", "Bm", "CM", "Cm", "DM", "Dm", "EM", "Em", "F#M", "F#m")); 
+				//D Major = two sharps 
+				keySignatures.add(new keySignature ("DM", "dM", "EM", "Em","F#M", "F#m", "GM", "Gm","AM", "Am", "BM", "Bm","C#M", "C#m"));
+				//A Major = three sharps 
+				keySignatures.add(new keySignature ("AM", "Am", "BM", "Bm", "C#M", "C#m", "DM", "Dm", "EM", "Em", "F#M", "F#m", "G#M", "G#m"));
 				
 				ChordGeneratorGreetUser.introduceGenerator(); 
+				ChordGeneratorGreetUser.askIfWantInstructions(); 			
 				
 				
 			}
--- Chord Generator/src/ChordGeneratorGreetUser.java
+++ Chord Generator/src/ChordGeneratorGreetUser.java
@@ -6,9 +6,79 @@ public class ChordGeneratorGreetUser
 		
 		public static void introduceGenerator() 
 		{ 
-			System.out.println("Hello, what is your name?");
+			System.out.println("Hello, welcome to the chord generator. What is your name?");
 			String name = userInput.nextLine();
-			System.out.println("Hello " + name);
+			System.out.println("Hello " + name.substring(0,1).toUpperCase() + name.substring(1) + ". " + "(Press enter to continue)");
+			userInput.nextLine(); 
+		}
+		
+		public static void askIfWantInstructions()
+		{
+			boolean userInputError = true;
+			while (userInputError)
+				{
+
+					System.out.println("Would you like the instructions?\n (1) Yes\n (2) No");
+					String wantInstructions = userInput.nextLine();
+
+					if (wantInstructions.equals("1") || wantInstructions.toUpperCase().equals("ONE"))
+						{
+							userInputError = false;
+							giveInstructions(); 
+						}
+
+					else if (wantInstructions.equals("2") || wantInstructions.toUpperCase().equals("TWO"))
+						{
+							System.out.println("Ok, well you must be a master. Lets play. ");
+							userInputError = false;
+						}
+
+					else
+						{
+							System.out.println("You only have two options.");
+						}
+				}	
+		} 
+		
+		public static void giveInstructions() 
+		{ 
+			System.out.println("Okay, here are the rules:");
+		}
+			
+			
+		
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			/*if (name.toUpperCase().equals("RYAN"))
+					{
+						System.out.println("You SUCK!");
+					}*/
+			
 			
 		}
-	}
+	
