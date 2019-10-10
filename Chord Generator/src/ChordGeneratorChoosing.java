0355ba600048fc6d6650c350dce2edcbb68fbfbd
ClancyMeade <cclancymeade@gmail.com>
2019-10-10 07:03:34 -0600

    Commit From Home 10/10/19 7:03, finished loop to print key signatures

--- Chord Generator/src/ChordGeneratorChoosing.java
+++ Chord Generator/src/ChordGeneratorChoosing.java
@@ -1,41 +1,36 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
-public class ChordGeneratorChoosing
-	{
-		static Scanner userInput = new Scanner(System.in);
-		
-	
-		
-		public static void chooseKeySignature()
-		{ 
-			System.out.println("Please choose a key signature below:"); 
-			System.out.println("(1) CMajor \n(2) GMajor \n(3) DMajor \n(4) AMajor");
-		
-			
-			int chosenKeySignatureIndex = userInput.nextInt(); 
-			
-			for (int j = 0; j < ChordGeneratorDriver.keySignatures.size(); j++)
-			{  
-				ChordGeneratorDriver.keySignatures.get(j).setIndex(j+1);
-				
-				int definedKeySignatureIndex = ChordGeneratorDriver.keySignatures.get(j).getIndex();
-					
-					if (chosenKeySignatureIndex == definedKeySignatureIndex)
-					{ 
-						userInput.nextLine(); 
-						System.out.println("Okay, you chose the key of: " + ChordGeneratorDriver.keySignatures.get(j).getMyKey());
-					}
+public class ChordGeneratorChoosing {
+	static Scanner userInput = new Scanner(System.in);
+
+	public static void chooseKeySignature() {
+		System.out.println("Please choose a key signature below:");
+
+		for (int c = 0; c < ChordGeneratorDriver.keySignatures.size(); c++) {
+			System.out.println("(" + (c + 1) + ") " + ChordGeneratorDriver.keySignatures.get(c).getMyKey());
+
+		}
+
+		int chosenKeySignatureIndex = userInput.nextInt();
+
+		for (int j = 0; j < ChordGeneratorDriver.keySignatures.size(); j++) {
+			ChordGeneratorDriver.keySignatures.get(j).setIndex(j + 1);
+
+			int definedKeySignatureIndex = ChordGeneratorDriver.keySignatures.get(j).getIndex();
+
+			if (chosenKeySignatureIndex == definedKeySignatureIndex) {
+				userInput.nextLine();
+				System.out
+						.println("Okay, you chose the key of: " + ChordGeneratorDriver.keySignatures.get(j).getMyKey());
 			}
 		}
-		
-		public static void chooseChordProgression()
-		{ 	
-			userInput.nextLine(); 
-			System.out.println("Now please choose a chord progression below:"); 
-			System.out.println("I, IV, V, II");
-			String inputChordProgression = userInput.nextLine(); 
-		}
-		
-		
-		
 	}
+
+	public static void chooseChordProgression() {
+		userInput.nextLine();
+		System.out.println("Now please choose a chord progression below:");
+		System.out.println("I, IV, V, II");
+		String inputChordProgression = userInput.nextLine();
+	}
+
+}
